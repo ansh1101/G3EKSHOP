bbc6142a15dd44f8637dbf4a8931941b784afd9b
ansh1101 <81492907+ansh1101@users.noreply.github.com>
2021-04-09 04:07:09 +0530

    wesite

---
+++ main.js
@@ -0,0 +1,10 @@
+
+function myFunction() {
+    var txt;
+    if (confirm("Press a button!")) {
+      txt = "You pressed OK!";
+    } else {
+      txt = "You pressed Cancel!";
+    }
+    document.getElementById("demo").innerHTML = txt;
+  }
