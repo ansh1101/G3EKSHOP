26acf563c0efc3c24694c0be712148f39b78ca7c
ansh1101 <81492907+ansh1101@users.noreply.github.com>
2021-04-10 11:25:08 +0530

    Add files via upload

---
+++ scrpt.js
@@ -0,0 +1,26 @@
+
+
+  var MenuItems= document.getElementById("MenuItems");
+
+  MenuItems.style.maxHeight ="0px";
+
+  function menutoggle(){
+      if(MenuItems.style.maxHeight =="0px")
+      {
+          MenuItems.style.maxHeight ="200px";
+      }
+      else
+      {
+          MenuItems.style.maxHeight ="0px";
+      }
+
+  }
+ 
+
+  function myFunction() {
+    document.getElementById("demo").innerHTML = "Added To Cart";
+  }
+  
+  function myFunction1() {
+    document.getElementById("demo").innerHTML = "Added To Cart";
+  }
