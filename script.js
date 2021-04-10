955a8f739da51f3e198deb5ac00dd5db4623b968
ansh1101 <81492907+ansh1101@users.noreply.github.com>
2021-04-10 22:47:31 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,21 +1,5 @@
 
 
-  var MenuItems= document.getElementById("MenuItems");
-
-  MenuItems.style.maxHeight ="0px";
-
-  function menutoggle(){
-      if(MenuItems.style.maxHeight =="0px")
-      {
-          MenuItems.style.maxHeight ="200px";
-      }
-      else
-      {
-          MenuItems.style.maxHeight ="0px";
-      }
-
-  }
- 
 
   function myFunction() {
     document.getElementById("demo").innerHTML = "Added To Cart";
@@ -24,3 +8,4 @@
   function myFunction1() {
     document.getElementById("demo").innerHTML = "Added To Cart";
   }
+
