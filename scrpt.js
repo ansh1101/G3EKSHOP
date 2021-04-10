455b63e0bc1ce2db06417699852f076d08a25927
ansh1101 <81492907+ansh1101@users.noreply.github.com>
2021-04-10 19:24:58 +0530

    Add files via upload

---
+++ scrpt.js
@@ -0,0 +1,11 @@
+
+
+
+  function myFunction() {
+    document.getElementById("demo").innerHTML = "Added To Cart";
+  }
+  
+  function myFunction1() {
+    document.getElementById("demo").innerHTML = "Added To Cart";
+  }
+
