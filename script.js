25bc2d221b93259ef734fb593ccf4cd9daa051b0
ansh1101 <81492907+ansh1101@users.noreply.github.com>
2021-04-11 19:08:48 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,42 @@
 
 
 
-  function myFunction() {
-    document.getElementById("demo").innerHTML = "Added To Cart";
+  function mySwitch() {
+    document.getElementById("switch").innerHTML = "Added To Cart";
   }
   
-  function myFunction1() {
-    document.getElementById("demo").innerHTML = "Added To Cart";
+  function myFunction() {
+    document.getElementById("demo0").innerHTML = "Added To Cart";
+  }
+
+  function my1() {
+    document.getElementById("demo1").innerHTML = "Added To Cart";
+  }
+  function my2() {
+    document.getElementById("demo2").innerHTML = "Added To Cart";
+  }
+  function my3() {
+    document.getElementById("demo3").innerHTML = "Added To Cart";
+  }
+  function my4() {
+    document.getElementById("demo4").innerHTML = "Added To Cart";
+  }
+  function my5() {
+    document.getElementById("demo5").innerHTML = "Added To Cart";
+  }
+  function my6() {
+    document.getElementById("demo6").innerHTML = "Added To Cart";
+  }
+  function my7() {
+    document.getElementById("demo7").innerHTML = "Added To Cart";
+  }
+  function my8() {
+    document.getElementById("demo8").innerHTML = "Added To Cart";
+  }
+  function my9() {
+    document.getElementById("demo9").innerHTML = "Added To Cart";
+  }
+  function my10() {
+    document.getElementById("demo10").innerHTML = "Added To Cart";
   }
 
